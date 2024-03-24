886e82e4efb76106dd01737083485c860f0980a6
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-03-24 14:48:23 +0530

    new commit

--- frontend/src/config/serverURL.js
+++ frontend/src/config/serverURL.js
@@ -1,5 +1,5 @@
 // const SERVER_URL ="http://localhost:3001/api"; // Replace with your actual server URL
-const SERVER_URL ="https://cloud-kitchen-wmr1.onrender.com/api"; // Replace with your actual server URL
+//const SERVER_URL ="https://cloud-kitchen-wmr1.onrender.com/api"; // Replace with your actual server URL
 // const SERVER_URL ="http://3.6.117.178/api"; // Replace with your actual server URL
-
+const SERVER_URL ="https://cloud-kitchen.sanjaysanthosh.me/api";
 export default SERVER_URL;
